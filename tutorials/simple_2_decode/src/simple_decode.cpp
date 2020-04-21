1a4f8c303cfe6c5b8465a56cc7ac2006fa556ac8
Kevin Han <kevin.han@3xlogic.cn>
2020-04-21 16:53:07 +0800

    test 1.

--- tutorials/simple_2_decode/src/simple_decode.cpp
+++ tutorials/simple_2_decode/src/simple_decode.cpp
@@ -20,263 +20,41 @@
 
 #include "common_utils.h"
 #include "cmd_options.h"
+#include "..\hevcd\hevcd.h"
 
-static void usage(CmdOptionsCtx* ctx)
+int main()
 {
-    printf(
-        "Decodes INPUT and optionally writes OUTPUT.\n"
-        "\n"
-        "Usage: %s [options] INPUT [OUTPUT]\n", ctx->program);
-}
+    LPCTSTR DLL_HEVCD = "hevcd.dll";
+    HINSTANCE hEVCD = ::LoadLibraryEx(DLL_HEVCD, NULL, LOAD_WITH_ALTERED_SEARCH_PATH);
+    if (hEVCD)
+    {
+        typedef void* (*dll_create_hevcd)();
+        typedef void (*dll_destroy_hevcd)(void* p);
 
-int main(int argc, char* argv[])
-{
-    mfxStatus sts = MFX_ERR_NONE;
-    bool bEnableOutput; // if true, removes all YUV file writing
-    CmdOptions options;
+        typedef int	(*dll_decode)(void* p, unsigned char* lpbFrame, long nFrameSize);
+        typedef unsigned char* (*dll_GetFrame)(void* p, void*& lpbi);
 
-    // =====================================================================
-    // Intel Media SDK decode pipeline setup
-    // - In this example we are decoding an AVC (H.264) stream
-    // - For simplistic memory management, system memory surfaces are used to store the decoded frames
-    //   (Note that when using HW acceleration video surfaces are prefered, for better performance)
-    //
+        dll_create_hevcd create_hevcd = reinterpret_cast<dll_create_hevcd>(::GetProcAddress(hEVCD, "create_hevcd"));
+        dll_destroy_hevcd destroy_hevcd = reinterpret_cast<dll_destroy_hevcd>(::GetProcAddress(hEVCD, "destroy_hevcd"));
+        dll_decode decode = reinterpret_cast<dll_decode>(::GetProcAddress(hEVCD, "decode"));
+        dll_GetFrame GetFrame = reinterpret_cast<dll_GetFrame>(::GetProcAddress(hEVCD, "GetFrame"));
 
-    // Read options from the command line (if any is given)
-    memset(&options, 0, sizeof(CmdOptions));
-    options.ctx.options = OPTIONS_DECODE;
-    options.ctx.usage = usage;
-    // Set default values:
-    options.values.impl = MFX_IMPL_SOFTWARE;
+        constexpr int BUF_LEN = 1024 * 1024;
+        LPBYTE buf = (LPBYTE)realloc(nullptr, BUF_LEN);
 
-    // here we parse options
-    ParseOptions(argc, argv, &options);
+        fileUniPtr fSource(OpenFile(R"(D:\Kevin\h265_2.h265)", "rb"), &CloseFile);
+        fread(buf, 1, BUF_LEN, fSource.get());
 
-    if (!options.values.SourceName[0]) {
-        printf("error: source file name not set (mandatory)\n");
-        return -1;
+        void* hdl = create_hevcd();
+
+        int ret = decode(hdl, buf, BUF_LEN);
+        LPBITMAPINFO lpbmpinfo = nullptr;
+        LPBYTE frmdata = GetFrame(hdl, (void*&)lpbmpinfo);
+
+        destroy_hevcd(hdl);
+
+        free(buf);
     }
-
-    bEnableOutput = (options.values.SinkName[0] != '\0');
-    // Open input H.264 elementary stream (ES) file
-    fileUniPtr fSource(OpenFile(options.values.SourceName, "rb"), &CloseFile);
-    MSDK_CHECK_POINTER(fSource, MFX_ERR_NULL_PTR);
-    // Create output YUV file
-    fileUniPtr fSink(nullptr, &CloseFile);
-    if (bEnableOutput) {
-        fSink.reset(OpenFile(options.values.SinkName, "wb"));
-        MSDK_CHECK_POINTER(fSink, MFX_ERR_NULL_PTR);
-    }
-
-    // Initialize Intel Media SDK session
-    // - MFX_IMPL_AUTO_ANY selects HW acceleration if available (on any adapter)
-    // - Version 1.0 is selected for greatest backwards compatibility.
-    // OS specific notes
-    // - On Windows both SW and HW libraries may present
-    // - On Linux only HW library only is available
-    //   If more recent API features are needed, change the version accordingly
-    mfxIMPL impl = options.values.impl;
-    mfxVersion ver = { {0, 1} };
-    MFXVideoSession session;
-
-    sts = Initialize(impl, ver, &session, NULL);
-    MSDK_CHECK_RESULT(sts, MFX_ERR_NONE, sts);
-
-    // Create Media SDK decoder
-    MFXVideoDECODE mfxDEC(session);
-
-    // Set required video parameters for decode
-    mfxVideoParam mfxVideoParams;
-    memset(&mfxVideoParams, 0, sizeof(mfxVideoParams));
-    mfxVideoParams.mfx.CodecId = MFX_CODEC_HEVC;
-    mfxVideoParams.IOPattern = MFX_IOPATTERN_OUT_SYSTEM_MEMORY;
-
-    // 4. Load the HEVC plugin
-    mfxPluginUID codecUID;
-    bool success = true;
-    codecUID = msdkGetPluginUID(impl, MSDK_VDECODE, mfxVideoParams.mfx.CodecId);
-
-    if (AreGuidsEqual(codecUID, MSDK_PLUGINGUID_NULL)) {
-        printf("Get Plugin UID for HEVC is failed.\n");
-        success = false;
-    }
-
-    printf("Loading HEVC plugin: %s\n", ConvertGuidToString(codecUID));
-
-    //On the success of get the UID, load the plugin
-    if (success) {
-        sts = MFXVideoUSER_Load(session, &codecUID, ver.Major);
-        if (sts < MFX_ERR_NONE) {
-            printf("Loading HEVC plugin failed\n");
-            success = false;
-        }
-    }
-
-    // Prepare Media SDK bit stream buffer
-    // - Arbitrary buffer size for this example
-    mfxBitstream mfxBS;
-    memset(&mfxBS, 0, sizeof(mfxBS));
-    mfxBS.MaxLength = 1024 * 1024;
-    std::vector<mfxU8> bstData(mfxBS.MaxLength);
-    mfxBS.Data = bstData.data();
-
-
-    // Read a chunk of data from stream file into bit stream buffer
-    // - Parse bit stream, searching for header and fill video parameters structure
-    // - Abort if bit stream header is not found in the first bit stream buffer chunk
-    sts = ReadBitStreamData(&mfxBS, fSource.get());
-    MSDK_CHECK_RESULT(sts, MFX_ERR_NONE, sts);
-
-    sts = mfxDEC.DecodeHeader(&mfxBS, &mfxVideoParams);
-    MSDK_IGNORE_MFX_STS(sts, MFX_WRN_PARTIAL_ACCELERATION);
-    MSDK_CHECK_RESULT(sts, MFX_ERR_NONE, sts);
-
-    // Validate video decode parameters (optional)
-    // sts = mfxDEC.Query(&mfxVideoParams, &mfxVideoParams);
-
-    // Query number of required surfaces for decoder
-    mfxFrameAllocRequest Request;
-    memset(&Request, 0, sizeof(Request));
-    sts = mfxDEC.QueryIOSurf(&mfxVideoParams, &Request);
-    MSDK_IGNORE_MFX_STS(sts, MFX_WRN_PARTIAL_ACCELERATION);
-    MSDK_CHECK_RESULT(sts, MFX_ERR_NONE, sts);
-
-    mfxU16 numSurfaces = Request.NumFrameSuggested;
-
-    // Allocate surfaces for decoder
-    // - Width and height of buffer must be aligned, a multiple of 32
-    // - Frame surface array keeps pointers all surface planes and general frame info
-    mfxU16 width = (mfxU16) MSDK_ALIGN32(Request.Info.Width);
-    mfxU16 height = (mfxU16) MSDK_ALIGN32(Request.Info.Height);
-    mfxU8 bitsPerPixel = 12;        // NV12 format is a 12 bits per pixel format
-    mfxU32 surfaceSize = width * height * bitsPerPixel / 8;
-    std::vector<mfxU8> surfaceBuffersData(surfaceSize * numSurfaces);
-    mfxU8* surfaceBuffers = surfaceBuffersData.data();
-
-    // Allocate surface headers (mfxFrameSurface1) for decoder
-    std::vector<mfxFrameSurface1> pmfxSurfaces(numSurfaces);
-    for (int i = 0; i < numSurfaces; i++) {
-        memset(&pmfxSurfaces[i], 0, sizeof(mfxFrameSurface1));
-        pmfxSurfaces[i].Info = mfxVideoParams.mfx.FrameInfo;
-        pmfxSurfaces[i].Data.Y = &surfaceBuffers[surfaceSize * i];
-        pmfxSurfaces[i].Data.U = pmfxSurfaces[i].Data.Y + width * height;
-        pmfxSurfaces[i].Data.V = pmfxSurfaces[i].Data.U + 1;
-        pmfxSurfaces[i].Data.Pitch = width;
-    }
-
-    // Initialize the Media SDK decoder
-    sts = mfxDEC.Init(&mfxVideoParams);
-    MSDK_IGNORE_MFX_STS(sts, MFX_WRN_PARTIAL_ACCELERATION);
-    MSDK_CHECK_RESULT(sts, MFX_ERR_NONE, sts);
-
-    // ===============================================================
-    // Start decoding the frames
-    //
-
-    mfxTime tStart, tEnd;
-    mfxGetTime(&tStart);
-
-    mfxSyncPoint syncp;
-    mfxFrameSurface1* pmfxOutSurface = NULL;
-    int nIndex = 0;
-    mfxU32 nFrame = 0;
-
-    //
-    // Stage 1: Main decoding loop
-    //
-    while (MFX_ERR_NONE <= sts || MFX_ERR_MORE_DATA == sts || MFX_ERR_MORE_SURFACE == sts) {
-        if (MFX_WRN_DEVICE_BUSY == sts)
-            MSDK_SLEEP(1);  // Wait if device is busy, then repeat the same call to DecodeFrameAsync
-
-        if (MFX_ERR_MORE_DATA == sts) {
-            sts = ReadBitStreamData(&mfxBS, fSource.get());       // Read more data into input bit stream
-            MSDK_BREAK_ON_ERROR(sts);
-        }
-
-        if (MFX_ERR_MORE_SURFACE == sts || MFX_ERR_NONE == sts) {
-            nIndex = GetFreeSurfaceIndex(pmfxSurfaces);        // Find free frame surface
-            MSDK_CHECK_ERROR(MFX_ERR_NOT_FOUND, nIndex, MFX_ERR_MEMORY_ALLOC);
-        }
-        // Decode a frame asychronously (returns immediately)
-        //  - If input bitstream contains multiple frames DecodeFrameAsync will start decoding multiple frames, and remove them from bitstream
-        sts = mfxDEC.DecodeFrameAsync(&mfxBS, &pmfxSurfaces[nIndex], &pmfxOutSurface, &syncp);
-
-        // Ignore warnings if output is available,
-        // if no output and no action required just repeat the DecodeFrameAsync call
-        if (MFX_ERR_NONE < sts && syncp)
-            sts = MFX_ERR_NONE;
-
-        if (MFX_ERR_NONE == sts)
-            sts = session.SyncOperation(syncp, 60000);      // Synchronize. Wait until decoded frame is ready
-
-        if (MFX_ERR_NONE == sts) {
-            ++nFrame;
-            if (bEnableOutput) {
-                sts = WriteRawFrame(pmfxOutSurface, fSink.get());
-                MSDK_BREAK_ON_ERROR(sts);
-                fflush(fSink.get());
-
-                printf("Frame number: %d\r", nFrame);
-                fflush(stdout);
-            }
-        }
-    }
-
-    // MFX_ERR_MORE_DATA means that file has ended, need to go to buffering loop, exit in case of other errors
-    MSDK_IGNORE_MFX_STS(sts, MFX_ERR_MORE_DATA);
-    MSDK_CHECK_RESULT(sts, MFX_ERR_NONE, sts);
-
-    //
-    // Stage 2: Retrieve the buffered decoded frames
-    //
-    while (MFX_ERR_NONE <= sts || MFX_ERR_MORE_SURFACE == sts) {
-        if (MFX_WRN_DEVICE_BUSY == sts)
-            MSDK_SLEEP(1);  // Wait if device is busy, then repeat the same call to DecodeFrameAsync
-
-        nIndex = GetFreeSurfaceIndex(pmfxSurfaces);        // Find free frame surface
-        MSDK_CHECK_ERROR(MFX_ERR_NOT_FOUND, nIndex, MFX_ERR_MEMORY_ALLOC);
-
-        // Decode a frame asychronously (returns immediately)
-        sts = mfxDEC.DecodeFrameAsync(NULL, &pmfxSurfaces[nIndex], &pmfxOutSurface, &syncp);
-
-        // Ignore warnings if output is available,
-        // if no output and no action required just repeat the DecodeFrameAsync call
-        if (MFX_ERR_NONE < sts && syncp)
-            sts = MFX_ERR_NONE;
-
-        if (MFX_ERR_NONE == sts)
-            sts = session.SyncOperation(syncp, 60000);      // Synchronize. Waits until decoded frame is ready
-
-        if (MFX_ERR_NONE == sts) {
-            ++nFrame;
-            if (bEnableOutput) {
-                sts = WriteRawFrame(pmfxOutSurface, fSink.get());
-                MSDK_BREAK_ON_ERROR(sts);
-
-                printf("Frame number: %d\r", nFrame);
-                fflush(stdout);
-            }
-        }
-    }
-
-    // MFX_ERR_MORE_DATA indicates that all buffers has been fetched, exit in case of other errors
-    MSDK_IGNORE_MFX_STS(sts, MFX_ERR_MORE_DATA);
-    MSDK_CHECK_RESULT(sts, MFX_ERR_NONE, sts);
-
-    mfxGetTime(&tEnd);
-    double elapsed = TimeDiffMsec(tEnd, tStart) / 1000;
-    double fps = ((double)nFrame / elapsed);
-    printf("\nExecution time: %3.2f s (%3.2f fps)\n", elapsed, fps);
-
-    // ===================================================================
-    // Clean up resources
-    //  - It is recommended to close Media SDK components first, before releasing allocated surfaces, since
-    //    some surfaces may still be locked by internal Media SDK resources.
-
-    mfxDEC.Close();
-    // session closed automatically on destruction
-
-    Release();
 
     return 0;
 }
